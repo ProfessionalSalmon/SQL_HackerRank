f5bd9552ff7b07ceb0bfc9f5b0006c94cf5da577
ProfessionalSalmon <tterrakkee@gmail.com>
2024-07-28 00:15:12 +0700

    weather20

---
+++ Medium/Weather_Observation_Station20.sql
@@ -0,0 +1,21 @@
+SELECT
+   CAST(AVG(LAT_N) AS DECIMAL(10, 4))
+FROM
+   (
+      SELECT
+         LAT_N,
+         ROW_NUMBER() OVER (
+            ORDER BY
+               LAT_N ASC,
+               id ASC
+         ) AS row_asc,
+         ROW_NUMBER() OVER (
+            ORDER BY
+               LAT_N DESC,
+               id DESC
+         ) AS row_desc
+      FROM
+         STATION
+   ) AS t1
+WHERE
+   row_asc IN (row_desc, row_desc - 1, row_desc + 1);
