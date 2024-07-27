61ed0ebe357aa4de52fac250bb834075c7403fd6
ProfessionalSalmon <tterrakkee@gmail.com>
2024-07-27 22:35:01 +0700

    format

--- Medium/Contest_Leaderboard/Solution1.sql
+++ Medium/Contest_Leaderboard/Solution1.sql
@@ -1,11 +1,28 @@
 --MAX()
-
-SELECT hacker_id, name, SUM(max_score) as sum_score
-FROM 
-(SELECT h.hacker_id AS hacker_id, name, challenge_id, MAX(score) AS max_score
-FROM HACKERS h
-    JOIN SUBMISSIONS s ON h.hacker_id = s.hacker_id
-GROUP BY h.hacker_id, name, challenge_id) AS t1
-GROUP BY hacker_id, name
-HAVING SUM(max_score) != 0
-ORDER BY SUM(max_score) DESC, hacker_id
+SELECT
+    hacker_id,
+    name,
+    SUM(max_score) as sum_score
+FROM
+    (
+        SELECT
+            h.hacker_id AS hacker_id,
+            name,
+            challenge_id,
+            MAX(score) AS max_score
+        FROM
+            HACKERS h
+            JOIN SUBMISSIONS s ON h.hacker_id = s.hacker_id
+        GROUP BY
+            h.hacker_id,
+            name,
+            challenge_id
+    ) AS t1
+GROUP BY
+    hacker_id,
+    name
+HAVING
+    SUM(max_score) != 0
+ORDER BY
+    SUM(max_score) DESC,
+    hacker_id
