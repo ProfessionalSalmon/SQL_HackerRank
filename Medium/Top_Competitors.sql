e52f3407ecb66030dd7145c5cc67bf496a212edc
ProfessionalSalmon <tterrakkee@gmail.com>
2024-07-27 21:47:45 +0700

    top_com

---
+++ Medium/Top_Competitors.sql
@@ -0,0 +1,18 @@
+WITH t1 AS
+(SELECT h.hacker_id
+ , h.name
+ , c.challenge_id
+ , c.difficulty_level
+ , s.score AS score_obtained
+ , d.score AS full_score
+FROM Hackers h
+    JOIN Submissions s ON h.hacker_id = s.hacker_id
+    JOIN Challenges c ON s.challenge_id = c.challenge_id
+    JOIN Difficulty d ON d.difficulty_level = c.difficulty_level
+WHERE s.score = d.score)
+
+SELECT hacker_id, name
+FROM t1
+GROUP BY hacker_id, name
+HAVING COUNT(*) > 1
+ORDER BY COUNT(*) DESC, hacker_id
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# SQL_HackerRank
-## MS SQL
+# SQL_HackerRank Solutions by Terra
+
+Welcome to Terra's space. Feel free to study from my codes and please let me know how I can improve my codes or if there is any issue.
+
+Note: All of the codes are MS SQL
