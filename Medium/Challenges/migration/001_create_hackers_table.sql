db1074ed5dbea57497b07b9ddb1ca5118841f601
ProfessionalSalmon <tterrakkee@gmail.com>
2024-07-28 16:48:32 +0700

    challenges_migration

---
+++ Medium/Challenges/migration/001_create_hackers_table.sql
@@ -0,0 +1,4 @@
+CREATE TABLE HACKERS (
+    hacker_id INT PRIMARY KEY,
+    name varchar(50) NOT NULL
+)
---
+++ Medium/Challenges/migration/002_create_challenges_table.sql
@@ -0,0 +1,4 @@
+CREATE TABLE CHALLENGES (
+    challenge_id INT,
+    hacker_id INT FOREIGN KEY REFERENCES Hackers(hacker_id)
+)
